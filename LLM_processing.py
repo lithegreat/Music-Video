8f2b33d3b166e470f395c6c4fcd2988ccfbd6279
Hengsheng Li <hengsheng.li@tum.de>
2024-06-21 16:16:23 +0200

    add lyrics-sync.py

--- LLM_processing.py
+++ LLM_processing.py
@@ -1,17 +1,17 @@
 import os
 from together import Together
-class LLM: 
-    def __init__(self, key, intel, topic): 
-        self.__key = "8069bcddb5b335ea3e2f23e9d58d83d5dfb270ee6ffcb8a546fbcdf8fb336dac" if key is None else key 
+class LLM:
+    def __init__(self, key, intel, topic):
+        self.__key = "8069bcddb5b335ea3e2f23e9d58d83d5dfb270ee6ffcb8a546fbcdf8fb336dac" if key is None else key
         self.__establishKey()
         self.__intel = "You are one of the best song writers in the world.\nKnowledge cutoff: 2021-09-01\nCurrent date: 2023-03-02" if intel is None else intel
-        self.__base_prompt =  f"""Write a set of compelling lyrics for a song with topic {topic} and perform it with your exceptional singing skills. 
-                                The lyrics should convey a meaningful message or tell a captivating story, and your performance should showcase your vocal 
-                                abilities and emotional expression. Your goal is to create an original and engaging musical piece that resonates with the audience. 
+        self.__base_prompt =  f"""Write a set of compelling lyrics for a song with topic {topic} and perform it with your exceptional singing skills.
+                                The lyrics should convey a meaningful message or tell a captivating story, and your performance should showcase your vocal
+                                abilities and emotional expression. Your goal is to create an original and engaging musical piece that resonates with the audience.
                                 Feel free to explore various themes and musical styles to demonstrate the depth of your talent as a lyricist and singer. """
         self.__together_client = Together(api_key=os.environ.get("TOGETHER_API_KEY"))
-    
-    def __establishKey(self): 
+
+    def __establishKey(self):
         os.environ['TOGETHER_API_KEY'] =  self.key
 
     def ask_llama_3_8b_stream_TOGETHER_API(self, prompt):
@@ -24,21 +24,31 @@ class LLM:
             stream=True,
         )
         response_message = chat_completion_stream_response['message']['content']
-        #For generating the 
+        #For generating the
         for chunk in chat_completion_stream_response:
             print(chunk.choices[0].delta.content or "", end="", flush=True)
-        
+
         return response_message
 
     def generateText(self, topic, visualize=True):
         prompt = self.__base_prompt.replace("topic", topic)
-        if visualize: 
+        if visualize:
             self.ask_llama_3_8b_stream_TOGETHER_API(prompt)
 
 
 
         return self.ask_llama_3_8b_TOGETHER_API(prompt)
-    
+
+    def save_data_to_txt(self, filename):
+        prompt = self.__base_prompt.replace("topic", topic)
+        data_to_save = self.ask_llama_3_8b_stream_TOGETHER_API(prompt)
+
+        with open(filename, 'w') as f:
+            f.write(data_to_save)
+
+        print(f"Data saved to {filename}")
+
+    save_data_to_txt('lyrics.txt')
 
 
 
@@ -47,14 +57,11 @@ class LLM:
 
 
 
-
-
-#ToDo add examples to the prompt 
+#ToDo add examples to the prompt
 topic = ""
-prompt = f"""Write a set of compelling lyrics for a song with topic {topic} and perform it with your exceptional singing skills. The lyrics should 
-convey a meaningful message or tell a captivating story, and your performance should showcase your vocal abilities and emotional expression. 
-Your goal is to create an original and engaging musical piece that resonates with the audience. Feel free to explore various themes and musical styles 
+prompt = f"""Write a set of compelling lyrics for a song with topic {topic} and perform it with your exceptional singing skills. The lyrics should
+convey a meaningful message or tell a captivating story, and your performance should showcase your vocal abilities and emotional expression.
+Your goal is to create an original and engaging musical piece that resonates with the audience. Feel free to explore various themes and musical styles
 to demonstrate the depth of your talent as a lyricist and singer.
 """
 lyrics = ask_llama_3_8b_TOGETHER_API(system_intel, prompt)
-
--- music_model.py
+++ music_model.py
@@ -1,21 +1,24 @@
-#Bring in the model this will automatically download the weights 
+#Bring in the model this will automatically download the weights
 from audiocraft.models import musicgen
 from audiocraft.utils.notebook import display_audio
 import torch
-class musicModel: 
+import soundfile as sf
+class musicModel:
     def __init__(self, model_type: str, audio_length):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model = musicgen.MusicGen.get_pretrained(model_type, device=self.device)
         self.audio_length = audio_length
         self.__set_audio_length()
-    
+
     def __set_audio_length(self):
         self.model.set_generation_params(duration=self.audio_length)
-    def generateAudio(self, instructions): 
-        res = self.model.generate([
-            instructions
-        ], 
-            progress=True)
-        self.__display_audio(res)
-    def __display_audio(audio): 
+    def generateAudio(self, instructions):
+        res = self.model.generate([instructions], progress=True)
+        audio_data = res[0]
+
+        output_file = "generated_audio.wav"
+        sf.write(output_file, audio_data, 32000, format='WAV', subtype='PCM_24')
+
+        self.__display_audio(audio_data)
+    def __display_audio(audio):
         display_audio(audio, 32000)
