5e7e318aa79e23011b0ac43b9f75f0621a1307ba
frg70 <fer.ramirez.gon@gmail.com>
2024-06-28 12:44:58 +0200

    Added the outputs of each of the models

--- music_model.py
+++ music_model.py
@@ -2,6 +2,11 @@
 from audiocraft.models import musicgen
 from audiocraft.utils.notebook import display_audio
 import torch
+import librosa
+from pydub import AudioSegment
+import io
+from gtts import gTTS
+
 class musicModel: 
     def __init__(self, model_type: str, audio_length):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -17,5 +22,51 @@ class musicModel:
         ], 
             progress=True)
         self.__display_audio(res)
-    def __display_audio(audio): 
+        self.save_audio(res[0], "output.mp3")
+        return res 
+    def __display_audio(self, audio): 
         display_audio(audio, 32000)
+
+    def generateVoice(self, lyrics):
+      tts = gTTS(lyrics, lang='en')
+      tts.save("voice.mp3")
+      return tts
+
+    def save_audio(self, res, filename):
+        # Convert the result to a byte stream
+        audio_buffer = io.BytesIO()
+        audio_segment = AudioSegment(
+            res.tobytes(), 
+            frame_rate=22050,  # Use the correct sample rate for your model's output
+            sample_width=res.dtype.itemsize, 
+            channels=1  # Assuming mono channel
+        )
+        audio_segment.export(audio_buffer, format="mp3")
+        
+        # Save the byte stream to a file
+        with open(filename, "wb") as f:
+            f.write(audio_buffer.getbuffer())
+    def extract_keynotes(self, audio):
+        music_file = "output.mp3"
+        y_music, sr_music = librosa.load(music_file, sr=None)
+
+        #Extract melody using librosa's piptrack
+        pitches, magnitudes = librosa.core.piptrack(y=y_music, sr=sr_music)
+
+        # Extract the predominant melody
+        melody = []
+        for t in range(pitches.shape[1]):
+            index = magnitudes[:, t].argmax()
+            pitch = pitches[index, t]
+            if pitch > 0:  # Ignore zero-pitch (no pitch detected)
+                melody.append(pitch)
+            else:
+                melody.append(0)
+
+        # Convert pitch to MIDI note numbers
+        melody_midi = librosa.hz_to_midi(melody)
+    def extract_keyPointsVoice(self, voice): 
+        voice_file = "voice.mp3"
+        y_voice, sr_voice = librosa.load(voice_file, sr=None)
+        phoneme_segments = librosa.effects.split(y_voice, top_db=30)
+        phonemes = [y_voice[start:end] for start, end in phoneme_segments]
